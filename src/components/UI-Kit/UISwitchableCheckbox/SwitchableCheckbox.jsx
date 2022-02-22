041d71b36515674649f08cca791de3f3a65e7cc6
KoTuK2306 <dmitriy_aleshin_00@mail.ru>
2022-02-22 22:04:12 +0300

    feat: improved switchable checkbox

--- src/components/UI-Kit/UISwitchableCheckbox/SwitchableCheckbox.jsx
+++ src/components/UI-Kit/UISwitchableCheckbox/SwitchableCheckbox.jsx
@@ -1,12 +1,53 @@
-import { StyledLabel, StyledCheckbox } from "./SwitchableCheckbox.style";
+import PropTypes from "prop-types";
+import { StyledLabel, StyledCheckbox, StyledDiv, Background, Circle } from "./SwitchableCheckbox.style";
 
-export const SwitchableCheckbox = () => {
+export const SwitchableCheckbox = ({
+  text,
+  textColor = "#000",
+  checkedCheckbox = "#7309AA",
+  uncheckedCheckbox = "#7E7E7E",
+  checkboxBackground = "#ddd",
+  size = "md",
+}) => {
+  let scale = 1;
+  if (size === "sm") scale = 0.5;
+  if (size === "lg") scale = 1.75;
+
   return (
-    <>
-      <StyledCheckbox type="checkbox" id="check"></StyledCheckbox>
-      <StyledLabel for="check" id="check">
-        Text for checkbox
+    <StyledDiv>
+      <StyledLabel scale={scale} textColor={textColor}>
+        <Background checkboxBackground={checkboxBackground} scale={scale} />
+        <StyledCheckbox checkedCheckbox={checkedCheckbox} scale={scale} type="checkbox" />
+        <Circle uncheckedCheckbox={uncheckedCheckbox} scale={scale} />
+        {text}
       </StyledLabel>
-    </>
+    </StyledDiv>
   );
+};
+
+SwitchableCheckbox.propTypes = {
+  /**
+   * Input lable text
+   */
+  text: PropTypes.string.isRequired,
+  /**
+   * Choose color for lable text
+   */
+  textColor: PropTypes.string,
+  /**
+   * Choose color for checked checkbox
+   */
+  checkedCheckbox: PropTypes.string,
+  /**
+   * Choose color for uncheckedCheckbox checkbox
+   */
+  uncheckedCheckbox: PropTypes.string,
+  /**
+   * Choose color for checkbox background
+   */
+  checkboxBackground: PropTypes.string,
+  /**
+   * Choose size of your switchable checkbox
+   */
+  size: PropTypes.oneOf(["sm", "md", "lg"]),
 };
--- src/components/UI-Kit/UISwitchableCheckbox/SwitchableCheckbox.style.js
+++ src/components/UI-Kit/UISwitchableCheckbox/SwitchableCheckbox.style.js
@@ -1,47 +1,57 @@
 import styled from "styled-components";
+import { rgba } from "polished";
+
+const checkboxWidth = 80;
+const checkboxHeight = 40;
+
+export const Circle = styled.div`
+  position: relative;
+  z-index: 10;
+  width: ${(props) => `${(checkboxWidth * props.scale) / 2 - 5}px`};
+  height: ${(props) => `${checkboxHeight * props.scale - 5}px`};
+  background-color: ${(props) => props.uncheckedCheckbox};
+  border-radius: 50%;
+  left: ${(props) => `${-1 * (checkboxWidth * props.scale - 2.5)}px`};
+  transition: all 0.3s;
+  margin-right: ${(props) => (props.scale === 0.5 ? `-5px` : `${-1 * (checkboxWidth / 2 - 5 - 10 * props.scale)}px`)};
+`;
+
+export const Background = styled.div`
+  position: relative;
+  z-index: -20;
+  background-color: ${(props) => props.checkboxBackground};
+  width: ${(props) => `${checkboxWidth * props.scale}px`};
+  height: ${(props) => `${checkboxHeight * props.scale}px`};
+  border-radius: ${(props) => (props.scale === 1.75 ? "40px" : `${checkboxWidth / 4}px`)};
+  transition: all 0.3s;
+`;
 
 export const StyledLabel = styled.label`
-  color: #aaa;
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: ${(props) => `${20 * props.scale}px`};
+  font-weight: 500;
+  color: ${(props) => props.textColor};
   cursor: pointer;
   user-select: none;
+
+  &:hover {
+    ${Background} {
+      box-shadow: ${`0px 0px 4px 3px ${rgba("#000", 0.5)} inset`};
+    }
+  }
 `;
 
 export const StyledCheckbox = styled.input`
-  opacity: 0;
-  position: absolute;
-  left: -90000px;
-  top: -90000px;
-
-  & + label {
-    position: relative;
-    display: flex;
-    font-size: 40px;
-    align-items: center;
+  display: none;
+  &:checked + ${Circle} {
+    left: ${(props) => `${-1 * ((checkboxWidth * props.scale) / 2 - 2.5)}px`};
+    background-color: ${(props) => props.checkedCheckbox};
   }
+`;
 
-  & + label::before {
-    content: "";
-    width: 80px;
-    height: 40px;
-    background-color: #ddd;
-    border-radius: 20px;
-    margin-right: 15px;
-  }
-
-  & + label::after {
-    position: absolute;
-    left: 2.5px;
-    content: "";
-    width: 35px;
-    height: 35px;
-    background-color: grey;
-    border-radius: 20px;
-    transition: all 0.5s;
-  }
-
-  &:checked + label::after {
-    left: 43px;
-    transform: translateX(0%);
-    background-color: purple;
-  }
+export const StyledDiv = styled.div`
+  display: flex;
 `;
